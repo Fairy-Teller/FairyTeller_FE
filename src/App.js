cdcac62ff9ea19e410196b1e68a05d716848d270
Kimjaejung <67777124+baebang@users.noreply.github.com>
2023-06-08 18:58:32 +0900

    [Add] 프론트엔드 폴더 구조 생성
    전체적인 폴더 페이지 구성 완료

--- src/App.js
+++ src/App.js
@@ -1,21 +1,5 @@
-import "./App.css";
-
-import { Button } from "@mui/material";
-import { signout } from "./service/UserService";
-
 function App() {
-  return (
-    <div className="App">
-      <Button
-        type="submit"
-        variant="contained"
-        color="primary"
-        onClick={signout}
-      >
-        로그아웃
-      </Button>
-    </div>
-  );
+    return <div className="App"></div>;
 }
 
 export default App;
--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,21 +1,23 @@
-import React from "react";
-import App from "./App";
-import Login from "./Login";
-import SignUp from "./SignUp";
-import SocialLogin from "./SocialLogin";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from 'react';
+import App from './App';
+import Login from './pages/user/Login';
+import SignUp from './pages/user/SignUp';
+import SocialLogin from './pages/user/SocialLogin';
+import Logout from './pages/user/Logout';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function AppRouter() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="login" element={<Login />} />
-        <Route path="signup" element={<SignUp />} />
-        <Route path="sociallogin" element={<SocialLogin />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App />} />
+                <Route path="login" element={<Login />} />
+                <Route path="signup" element={<SignUp />} />
+                <Route path="sociallogin" element={<SocialLogin />} />
+                <Route path="Logout" element={<Logout />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default AppRouter;
