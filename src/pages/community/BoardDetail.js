be3d75138d0859fce39142078667afb992c49fb5
flyduck-dev <flyduck0dev@gmail.com>
2023-06-10 05:11:10 +0900

    [Add]게시판 상세페이지 가운데 정렬

--- src/pages/community/BoardDetail.js
+++ src/pages/community/BoardDetail.js
@@ -24,12 +24,53 @@ const BoardDetail = () => {
   }
 
   return (
-    <div>
-      <h2>{board.title}</h2>
-      <p>Author: {board.author}</p>
-      <p>{board.content}</p>
+    <div style={styles.container}>
+      <h2 style={styles.title}>{board.title}</h2>
+      <div style={styles.center}>
+        <p style={styles.author}>Author: {board.author}</p>
+        <div style={styles.thumbnailContainer}>
+          <img src={board.thumbnailUrl} alt="Thumbnail" style={styles.thumbnail} />
+        </div>
+        <p style={styles.content}>{board.content}</p>
+      </div>
     </div>
   );
+};
+
+const styles = {
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  center: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: "24px",
+    fontWeight: "bold",
+    marginBottom: "10px",
+  },
+  author: {
+    fontSize: "16px",
+    color: "#666",
+    marginBottom: "5px",
+  },
+  thumbnailContainer: {
+    display: "flex",
+    justifyContent: "center",
+    marginBottom: "20px",
+  },
+  thumbnail: {
+    maxWidth: "100%",
+    maxHeight: "400px",
+  },
+  content: {
+    fontSize: "18px",
+    lineHeight: "1.4",
+  },
 };
 
 export default BoardDetail;
