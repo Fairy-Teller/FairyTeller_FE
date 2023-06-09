75698b4e112e75a263906f008d7269434e73f734
Kimjaejung <67777124+baebang@users.noreply.github.com>
2023-06-10 00:17:24 +0900

    [Add] 동화 완료 페이지
    디테일 수정 후, commit합니다

--- src/pages/fairytale/FairytaleShow.js
+++ src/pages/fairytale/FairytaleShow.js
@@ -2,19 +2,50 @@ import React from 'react';
 import Container from '../../components/layout/Container';
 import styled from 'styled-components';
 
-
 const BookCover = styled.div`
     width: 100vw;
     height: 100vh;
     background-image: url(https://raw.githubusercontent.com/roadmanfong/react-cropper/master/example/img/child.jpg);
+    background-size: cover;
 `;
-
+const FairytaleTitle = styled.div`
+    font-weight: 400;
+    font-size: 96px;
+    line-height: 162px;
+    text-align: left;
+    color: #ffffff;
+    padding-left: 5%;
+`;
+const ButtomFrame = styled.div`
+    width: 100vw;
+    display: flex;
+    justify-content: flex-end;
+    padding-right: 5%;
+    margin-bottom: 10px;
+`;
+const Button = styled.button`
+    width: 15%;
+    height: 104px;
+    left: 375px;
+    top: 873px;
+    background-color: white;
+    font-size: 150%;
+    border-radius: 51.5px;
+    margin-right: 1%;
+`;
 function FairytaleShow() {
     return (
         <div>
             <Container className={''}>
                 <BookCover>
-
+                    <div style={{ position: 'absolute', bottom: '0px' }}>
+                        <FairytaleTitle>My Little Fairytale</FairytaleTitle>
+                        <ButtomFrame>
+                            <Button>게시판 전시하기</Button>
+                            <Button>PDF로 내보내기</Button>
+                            <Button>동화책 보기</Button>
+                        </ButtomFrame>
+                    </div>
                 </BookCover>
             </Container>
         </div>
