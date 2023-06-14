62be8b6b3ea6ec92b2d7abdfbc10d25488a0843c
helen1031 <32950301+helen1031@users.noreply.github.com>
2023-06-15 03:56:20 +0900

    [Refacator] Sh 수정

--- scripts/before.sh
+++ scripts/before.sh
@@ -2,5 +2,7 @@
 
 cd /home/ubuntu/fe
 
+sudo pm2 delete fairyteller
+
 sudo rm -rf node_modules
 sudo rm package-lock.json
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -2,7 +2,7 @@
 
 cd /home/ubuntu/fe
 
-sudo pm2 delete fairyteller
+# sudo pm2 delete fairyteller
 
-sudo npm install
-sudo npm install --save react react-dom react-scripts react-particles-js
+# sudo npm install
+# sudo npm install --save react react-dom react-scripts react-particles-js
