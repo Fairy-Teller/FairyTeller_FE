3b1bc665823f96589d20f72f1e182949d9386572
Kimjaejung <67777124+baebang@users.noreply.github.com>
2023-06-15 02:39:16 +0900

    [Bug] 이미지가 제거되는 문제 이슈업

--- src/components/CanvasFabric.js
+++ src/components/CanvasFabric.js
@@ -1,205 +1,185 @@
-import React, { useState, useEffect } from "react";
-import { useRecoilState, useRecoilValue, useRecoilCallback } from "recoil";
-import { call } from "../service/ApiService";
-import { ImageState, StoryState } from "../recoil/Fairytailstate";
-import { fabric } from "fabric";
+import React, { useState, useEffect } from 'react';
+import { useRecoilState, useRecoilValue, useRecoilCallback } from 'recoil';
+import { call } from '../service/ApiService';
+import { ImageState, StoryState } from '../recoil/Fairytailstate';
+import { fabric } from 'fabric';
 
-const [canvasWidth, canvasHeight] = [1280, 720];
+const canvasWidth = 1280;
+const canvasHeight = 720;
 
 const CanvasFabric = () => {
-  const [fabricObjects, setFabricObjects] = useState([]);
-  const [storyTEXT, setStoryTEXT] = useRecoilState(StoryState);
-  const [imgURL, setImgURL] = useRecoilState(ImageState);
-  const [canvas, setCanvas] = useState(null);
+    const [fabricObjects, setFabricObjects] = useState([]);
+    const [storyText, setStoryText] = useRecoilState(StoryState);
+    const [imgURL, setImgURL] = useRecoilState(ImageState);
+    const [canvas, setCanvas] = useState(null);
+    const SRC_LINK = '/images/img-default.png';
+    const [contentImg, setContentImg] = useState(SRC_LINK);
 
-  useEffect(() => {
-    init();
-  }, []);
+    useEffect(() => {
+        init();
+    }, []);
 
-  const init = async () => {
-    try {
-      await getnewest();
-      await sendtext();
-    } catch (error) {
-      console.log(error);
-    } finally {
-      cinit();
-    }
-  };
+    useEffect(() => {
+        cinit();
+    }, [contentImg]);
 
-  const cinit = () => {
-    let canvas = new fabric.Canvas("c", {
-      width: canvasWidth,
-      height: canvasHeight,
-      backgroundColor: "pink",
-      opacity: 0.5,
-    });
+    console.log('랜더링이 몇번 되는가?');
 
-    const deftxt1 = new fabric.Text(storyTEXT.text, { left: 50, top: 140 });
-    // const deftxt2 = new fabric.Text(__________2, { left: 50, top: 350 });
-    // const deftxt3 = new fabric.Text(__________3, { left: 50, top: 500 });
-    canvas.add(deftxt1);
-    // canvas.add(deftxt2);
-    // canvas.add(deftxt3);
-
-    var rect1 = new fabric.Rect({
-      width: 200,
-      height: 100,
-      left: 700,
-      top: 50,
-      angle: 30,
-      fill: "rgba(255,0,0,0.5)",
-    });
-
-    var rect3 = new fabric.Rect({
-      width: 50,
-      height: 100,
-      left: 975,
-      top: 350,
-      angle: 45,
-      stroke: "#eee",
-      strokeWidth: 10,
-      fill: "rgba(0,0,200,0.5)",
-    });
-
-    var circle = new fabric.Circle({
-      radius: 50,
-      left: 975,
-      top: 75,
-      fill: "#aac",
-    });
-
-    var triangle = new fabric.Triangle({
-      width: 100,
-      height: 100,
-      left: 550,
-      top: 300,
-      fill: "#cca",
-    });
-
-    canvas.add(rect1, rect3, circle, triangle);
-
-    fabric.Image.fromURL(imgURL.url, (defimg, { imgsrc }) => {
-      if (defimg == null) {
-        alert("Error: No Default Image");
-      } else {
-        defimg.scale(0.75);
-        canvas.add(defimg);
-        setImgURL(imgsrc); // ???
-        canvas.renderAll();
-      }
-    });
-
-    const onChangDetect = (options) => {
-      options.target.setCoords();
-      canvas.forEachObject((obj) => {
-        if (obj === options.target) return;
-        obj.set("opacity", options.target.intersectsWithObject(obj) ? 0.75 : 1);
-      });
+    const init = async () => {
+        try {
+            await getNewest();
+            await sendText();
+        } catch (error) {
+            console.log(error);
+        }
     };
 
-    canvas.on({
-      "object:moving": onChangDetect,
-      "object:scaling": onChangDetect,
-      "object:rotating": onChangDetect,
-    });
+    const cinit = () => {
+        const canvas = new fabric.Canvas('c', {
+            width: canvasWidth,
+            height: canvasHeight,
+            backgroundColor: 'pink',
+            opacity: 0.5,
+        });
 
-    setCanvas(canvas);
-  };
+        const deftxt1 = new fabric.Text(storyText, { left: 50, top: 140 });
+        canvas.add(deftxt1);
 
-  const getnewest = async () => {
-    try {
-      const data = await call("/book/my-newest", "GET", null);
-      setStoryTEXT(data.fullStory);
-      setImgURL(data.thumbnailUrl);
-      //test
-      console.log(storyTEXT.text);
-      console.log(imgURL.url);
-    } catch (error) {
-      console.log("Error fetching data:", error);
-    }
-  };
+        var rect1 = new fabric.Rect({
+            width: 200,
+            height: 100,
+            left: 700,
+            top: 50,
+            angle: 30,
+            fill: 'rgba(255,0,0,0.5)',
+        });
 
-  const sendtext = useRecoilCallback(({ set }) => async (userDTO) => {
-    try {
-      const response = await call("/chat-gpt/summarize", "POST", userDTO);
+        var rect3 = new fabric.Rect({
+            width: 50,
+            height: 100,
+            left: 975,
+            top: 350,
+            angle: 45,
+            stroke: '#eee',
+            strokeWidth: 10,
+            fill: 'rgba(0,0,200,0.5)',
+        });
 
-      const imageData = response; // 응답 데이터 - Base64 문자열
-      const byteCharacters = atob(imageData); // Base64 디코딩
-      const byteArrays = [];
-      for (let i = 0; i < byteCharacters.length; i++) {
-        byteArrays.push(byteCharacters.charCodeAt(i));
-      }
+        var circle = new fabric.Circle({
+            radius: 50,
+            left: 975,
+            top: 75,
+            fill: '#aac',
+        });
 
-      const imageBlob = new Blob([new Uint8Array(byteArrays)], { type: "image/jpeg" });
-      const imageUrl = URL.createObjectURL(imageBlob);
-      await set(ImageState, { url: imageUrl });
-    } catch (error) {
-      console.log("Error fetching data:", error);
-    }
-  });
+        var triangle = new fabric.Triangle({
+            width: 100,
+            height: 100,
+            left: 550,
+            top: 300,
+            fill: '#cca',
+        });
 
-  const bringToFront = (e) => {
-    e.preventDefault();
-    let activeObj = canvas.getActiveObject();
-    activeObj && canvas.bringToFront(activeObj).discardActiveObject(activeObj).renderAll();
-  };
+        canvas.add(rect1, rect3, circle, triangle);
 
-  const exportSVG = (e) => {
-    e.preventDefault();
-    fabric.log("Normal SVG output: ", canvas.toSVG());
-  };
+        fabric.Image.fromURL(contentImg, (defimg) => {
+            console.log('contentImg 상태 >>>>>>>>>>>', contentImg);
+            if (defimg == null) {
+                alert('Error: No Default Image');
+            } else {
+                defimg.scale(0.75);
+                canvas.add(defimg);
+                setImgURL(defimg.toDataURL()); // 이미지 URL을 저장
+                canvas.renderAll();
+            }
+        });
 
-  const addRect = () => {
-    const rect = new fabric.Rect({
-      height: 30,
-      width: 300,
-      fill: "yellow",
-    });
+        const onChangeDetect = (options) => {
+            options.target.setCoords();
+            canvas.forEachObject((obj) => {
+                if (obj === options.target) return;
+                obj.set('opacity', options.target.intersectsWithObject(obj) ? 0.75 : 1);
+            });
+        };
 
-    canvas.add(rect);
-    setFabricObjects([...fabricObjects, rect]);
-    canvas.renderAll();
-  };
+        canvas.on({
+            'object:moving': onChangeDetect,
+            'object:scaling': onChangeDetect,
+            'object:rotating': onChangeDetect,
+        });
 
-  const addImg = (e) => {
-    e.preventDefault();
-    const { imgURL } = this.state;
-    new fabric.Image.fromURL(imgURL, (img) => {
-      img.scale(0.75);
-      canvas.add(img);
-      setFabricObjects([...fabricObjects, img]);
-      setImgURL("");
-    });
-    canvas.renderAll();
-  };
+        setCanvas(canvas);
+    };
 
-  // const handleChangeImgURL = (e) => {
-  //   setImgURL(e.target.value);
-  // };
+    const getNewest = async () => {
+        try {
+            const data = await call('/book/my-newest', 'GET', null);
+            setStoryText(data.fullStory);
+            setImgURL(data.thumbnailUrl);
+            console.log(storyText);
+            console.log(imgURL.url);
+        } catch (error) {
+            console.log('Error fetching data:', error);
+        }
+    };
 
-  return (
-    <div>
-      <canvas id='c' />
-      {/* <div>
-                <button onClick={bringToFront}>Bring to front</button>
-                <button onClick={exportSVG}>exportSVG</button>
-                <button onClick={addRect}>Rectangle</button>
-                <form onSubmit={addText}>
-                    <div>
-                        <input type="text" value={text} onChange={handleChangeText} />
-                        <button type="submit">Add Text</button>
-                    </div>
-                </form>
-                <form onSubmit={addImg}>
-                    <div>
-                        <input type="text" value={imgURL} onChange={handleChangeImgURL} />
-                        <button type="submit">Add Image</button>
-                    </div>
-                </form>
-            </div> */}
-    </div>
-  );
+    const sendText = async () => {
+        try {
+            const response = await call('/chat-gpt/summarize', 'POST', { text: storyText });
+            const imageData = response; // 응답 데이터 - Base64 문자열
+            const byteCharacters = atob(imageData); // Base64 디코딩
+            const byteArrays = new Uint8Array(byteCharacters.length);
+            for (let i = 0; i < byteCharacters.length; i++) {
+                byteArrays[i] = byteCharacters.charCodeAt(i);
+            }
+            const imageBlob = new Blob([byteArrays], { type: 'image/jpeg' });
+            const imageUrl = URL.createObjectURL(imageBlob);
+            setContentImg(imageUrl);
+            setImgURL(imageUrl);
+            // set(ImageState, { url: imageUrl });
+        } catch (error) {
+            console.log('Error fetching data:', error);
+        }
+    };
+
+    const bringToFront = (e) => {
+        e.preventDefault();
+        let activeObj = canvas.getActiveObject();
+        activeObj && canvas.bringToFront(activeObj).discardActiveObject(activeObj).renderAll();
+    };
+
+    const exportSVG = (e) => {
+        e.preventDefault();
+        fabric.log('Normal SVG output: ', canvas.toSVG());
+    };
+
+    const addRect = () => {
+        const rect = new fabric.Rect({
+            height: 30,
+            width: 300,
+            fill: 'yellow',
+        });
+        canvas.add(rect);
+        setFabricObjects([...fabricObjects, rect]);
+        canvas.renderAll();
+    };
+
+    const addImg = (e) => {
+        e.preventDefault();
+        new fabric.Image.fromURL(imgURL, (img) => {
+            img.scale(0.75);
+            canvas.add(img);
+            setFabricObjects([...fabricObjects, img]);
+            setImgURL('');
+        });
+        canvas.renderAll();
+    };
+
+    return (
+        <div>
+            <canvas id="c" />
+        </div>
+    );
 };
 
 export default CanvasFabric;
