7b1f892ee73d9eb35ea513b3226cfd0ee6ebd698
helen1031 <32950301+helen1031@users.noreply.github.com>
2023-06-15 00:15:33 +0900

    [Refactor] deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 
-npm install
-npm run build
-pm2 start npm --name "fairyteller" -- start
-npm save
+sudo npm install
+sudo npm run build
+sudo pm2 start npm --name "fairyteller" -- start
+sudo npm save
