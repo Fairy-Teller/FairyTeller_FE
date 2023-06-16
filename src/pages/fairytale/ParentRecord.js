ec15c7e60755be8f76f9ed0378175968ea617222
helen1031 <32950301+helen1031@users.noreply.github.com>
2023-06-17 02:18:02 +0900

    [Add] 사용자 목소리 녹음 기능(수정중)

---
+++ src/pages/fairytale/ParentRecord.js
@@ -0,0 +1,95 @@
+import React, { useState, useRef } from "react";
+import { sendAudioData } from "../../service/FairytaleService";
+
+const ParentRecord = () => {
+  const [isRecording, setIsRecording] = useState({});
+  const [recordings, setRecordings] = useState([]);
+  const mediaRecorder = useRef({});
+
+  const handleStartRecording = async (pageNumber) => {
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    mediaRecorder.current[pageNumber] = new MediaRecorder(stream);
+
+    const audioChunks = [];
+    mediaRecorder.current[pageNumber].ondataavailable = (event) => {
+      audioChunks.push(event.data);
+    };
+
+    mediaRecorder.current[pageNumber].onstop = () => {
+      const audioBlob = new Blob(audioChunks, { type: "audio/wav" });
+      setRecordings((prevRecordings) => [
+        ...prevRecordings,
+        { no: pageNumber, audio: audioBlob },
+      ]);
+    };
+
+    mediaRecorder.current[pageNumber].start();
+    setIsRecording((prevState) => ({ ...prevState, [pageNumber]: true }));
+  };
+
+  const handleStopRecording = (pageNumber) => {
+    mediaRecorder.current[pageNumber].stop();
+    setIsRecording((prevState) => ({ ...prevState, [pageNumber]: false }));
+  };
+
+  const convertBlobToBase64 = (blob) => {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onloadend = () => resolve(reader.result);
+      reader.onerror = reject;
+      reader.readAsDataURL(blob);
+    });
+  };
+
+  const handleSendRecording = async (recording) => {
+    try {
+      const base64Audio = await convertBlobToBase64(recording.audio);
+      const payload = {
+        bookId: 1,
+        pages: [
+          {
+            pageNo: recording.no,
+            audioUrl: base64Audio,
+          },
+        ],
+      };
+      sendAudioData(payload); // Make sure sendAudioData sends a POST request with Content-Type set to 'application/json'
+    } catch (error) {
+      console.error("Error converting audio blob to base64", error);
+    }
+  };
+
+  return (
+    <div>
+      {[1, 2, 3, 4, 5].map((pageNumber) => (
+        <div key={pageNumber}>
+          {!isRecording[pageNumber] && (
+            <button onClick={() => handleStartRecording(pageNumber)}>
+              녹음시작 {pageNumber}
+            </button>
+          )}
+          {isRecording[pageNumber] && (
+            <div>
+              <div>녹음중입니다... {pageNumber}</div>
+              <button onClick={() => handleStopRecording(pageNumber)}>
+                녹음종료
+              </button>
+            </div>
+          )}
+        </div>
+      ))}
+
+      {recordings.map((recording, index) => (
+        <div key={index}>
+          <p>Recording {recording.no}</p>
+          <audio src={URL.createObjectURL(recording.audio)} controls />
+          <button onClick={() => handleSendRecording(recording)}>
+            Send Recording {recording.no} to Backend
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ParentRecord;
--- src/service/FairytaleService.js
+++ src/service/FairytaleService.js
@@ -1,12 +1,17 @@
-import { call } from './ApiService';
-import { API_BASE_URL } from '../api-config';
+import { call } from "./ApiService";
+import { API_BASE_URL } from "../api-config";
 
 export function FairytaleNew(userDTO) {
-    return call('/book/my-newest', 'GET', userDTO).then((response) => {
-        //console.log("response: ", response);
-        //alert("token: " + response.token);
-        if (response) {
-            return response.json();
-        }
-    });
+  return call("/book/my-newest", "GET", userDTO).then((response) => {
+    //console.log("response: ", response);
+    //alert("token: " + response.token);
+    if (response) {
+      return response.json();
+    }
+  });
+}
+
+export function sendAudioData(audioDTO) {
+  console.log(audioDTO);
+  return call("/audio/user-record", "POST", audioDTO);
 }
