3fafbbbe617fa5ed5c4790ee045e10f76af520d6
Haebin <hbtopkr@gmail.com>
2023-06-21 03:15:32 +0900

    [Feat] Keyword Page redevelop - api related

--- src/components/LoadingModal.js
+++ src/components/LoadingModal.js
@@ -1,61 +1,56 @@
-import React,{useEffect} from 'react';
+import React, { useEffect } from "react";
 import styled from "styled-components";
 
-const Modal = styled.div`
-//   color: white;
-  width: 30%;
-  height: 30%;
+const ModalWrap = styled.div`
   display: flex;
-  opacity: 1;
-  justify-content: center; /* 수평 가운데 정렬 */
-  align-items: center; /* 수직 가운데 정렬 */
-  flex-direction: column; /* 요소를 세로로 배치 */
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  overflow: hidden;
+  z-index: 9999;
+  background-color: rgba(0, 0, 0, 0.4);
+`;
+
+const Modal = styled.div`
+  width: 36rem;
+  height: 24rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
   background-color: white;
-  border-radius: 50px; /* 모달 주위를 10px 둥글게 깎음 */
+  border-radius: 4rem;
 `;
 
 const MoveImg = styled.img`
-    width: 30%;
-    height: 30%;
-`;
-
-const Div = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center; /* 수평 가운데 정렬 */
-  align-items: center; /* 수직 가운데 정렬 */
-  position: absolute;
-  background-color: rgba(0, 0, 0, 0.5); /* 반투명한 검은색 배경 */
-  top: 0;
-  left: 0;
-  overflow: hidden; /* 스크롤 막기 */
-  z-index: 9999; /* 모달의 z-index 값을 높은 값으로 설정 */
+  width: 30%;
+  height: 30%;
+  margin: 0 0 2rem 0;
 `;
 
 const LoadingModal = ({ message }) => {
-  const handleScroll = (event) => {
-    event.preventDefault();
-  };
-
   useEffect(() => {
-    // 스크롤 이벤트 리스너 등록
-    window.addEventListener('scroll', handleScroll, { passive: false });
+    document.body.classList.add("noscroll");
 
-    // 컴포넌트 언마운트 시 스크롤 이벤트 리스너 해제
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      document.body.classList.remove("noscroll");
     };
   }, []);
 
-
   return (
-    <Div>
+    <ModalWrap>
       <Modal>
-        <MoveImg src="giphy.gif" alt="로딩 모달 이미지" />
-        <p>{ message }</p>
+        <MoveImg
+          src='giphy.gif'
+          alt='로딩 모달 이미지'
+        />
+        <p>{message}</p>
       </Modal>
-    </Div>
+    </ModalWrap>
   );
 };
 
--- src/recoil/Fairytailstate.js
+++ src/recoil/Fairytailstate.js
@@ -1,6 +1,6 @@
-import { atom, selector } from 'recoil';
-import { call } from '../service/ApiService';
-import { recoilPersist } from 'recoil-persist';
+import { atom, selector } from "recoil";
+import { call } from "../service/ApiService";
+import { recoilPersist } from "recoil-persist";
 
 const { persistAtom } = recoilPersist();
 
@@ -10,149 +10,154 @@ const { persistAtom } = recoilPersist();
 //   default: "default",
 // });
 
-export const SelectedKeywords = atom({ key: 'SelectedKeywords', default: [] });
+export const SelectedKeywords = atom({ key: "SelectedKeywords", default: [] });
 
 export const StoryState = atom({
-    key: 'StoryState',
-    default: [],
+  key: "StoryState",
+  default: [],
 });
+
 export const ImageTempState = atom({
-    key: 'ImageTempState',
-    default: [],
+  key: "ImageTempState",
+  default: [],
 });
 
 export const BookState = atom({
-    key: 'BookState',
-    default: {
-        bookId: null,
-        author: null,
-        title: null,
-        thumbnailUrl: '',
-        pages: [],
-    },
+  key: "BookState",
+  default: {
+    bookId: null,
+    author: null,
+    title: null,
+    thumbnailUrl: "",
+    pages: [],
+  },
 });
 
 export const ImageState = atom({
-    key: 'ImageState',
-    default: { url: '' },
+  key: "ImageState",
+  default: { url: "" },
 });
 
 export const ImageFix = atom({
-    key: 'userId',
-    default: '',
-    effects_UNSTABLE: [persistAtom],
+  key: "userId",
+  default: "",
+  effects_UNSTABLE: [persistAtom],
 });
 
 export const SelectedImageState = atom({
-    key: 'SelectedImageState',
-    default: [
-        {
-            id: 1,
-            src: './images/sample.jpg',
-        },
-        {
-            id: 2,
-            src: './images/sample.jpg',
-        },
-        {
-            id: 3,
-            src: './images/sample.jpg',
-        },
-        {
-            id: 4,
-            src: './images/sample.jpg',
-        },
-        {
-            id: 5,
-            src: './images/sample.jpg',
-        },
-    ],
+  key: "SelectedImageState",
+  default: [
+    {
+      id: 1,
+      src: "./images/sample.jpg",
+    },
+    {
+      id: 2,
+      src: "./images/sample.jpg",
+    },
+    {
+      id: 3,
+      src: "./images/sample.jpg",
+    },
+    {
+      id: 4,
+      src: "./images/sample.jpg",
+    },
+    {
+      id: 5,
+      src: "./images/sample.jpg",
+    },
+  ],
 });
 
 export const SampleDataState = atom({
-    key: 'SampleDataState',
-    default: [
+  key: "SampleDataState",
+  default: [
+    {
+      id: 1,
+      bookId: 1,
+      title: null,
+      thumbnailUrl: null,
+      pages: [
         {
-            id: 1,
-            bookId: 1,
-            title: null,
-            thumbnailUrl: null,
-            pages: [
-                {
-                    id: 1,
-                    src: './images/sample.jpg',
-                    sampledata: 'ivory',
-                },
-                {
-                    id: 2,
-                    src: './images/sample.png',
-                    sampledata: 'orange',
-                },
-                {
-                    id: 3,
-                    src: './images/img-default.png',
-                    sampledata: 'yellow',
-                },
-                {
-                    id: 4,
-                    src: './images/sample.jpg',
-                    sampledata: 'green',
-                },
-                {
-                    id: 5,
-                    src: './images/sample.png',
-                    sampledata: 'skyblue',
-                },
-            ],
+          id: 1,
+          src: "./images/sample.jpg",
+          sampledata: "ivory",
         },
-    ],
+        {
+          id: 2,
+          src: "./images/sample.png",
+          sampledata: "orange",
+        },
+        {
+          id: 3,
+          src: "./images/img-default.png",
+          sampledata: "yellow",
+        },
+        {
+          id: 4,
+          src: "./images/sample.jpg",
+          sampledata: "green",
+        },
+        {
+          id: 5,
+          src: "./images/sample.png",
+          sampledata: "skyblue",
+        },
+      ],
+    },
+  ],
 });
 
 export const SelectStickers = atom({
-    key: 'SelectStickers',
-    default: [
-        {
-            id: 1,
-            src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQshoMsYa1hFvjaMAwebiP2l99ssM_XWVrp7g&usqp=CAU',
-        },
-        {
-            id: 2,
-            src: 'https://fairyteller-dev.s3.ap-northeast-2.amazonaws.com/stickers/02.png',
-        },
-        {
-            id: 3,
-            src: 'https://fairyteller-dev.s3.ap-northeast-2.amazonaws.com/stickers/03.png',
-        },
-        {
-            id: 4,
-            src: 'https://fairyteller-dev.s3.ap-northeast-2.amazonaws.com/stickers/04.png',
-        },
-        {
-            id: 5,
-            src: 'https://fairyteller-dev.s3.ap-northeast-2.amazonaws.com/stickers/05.png',
-        },
-        {
-            id: 6,
-            src: 'https://fairyteller-dev.s3.ap-northeast-2.amazonaws.com/stickers/06.png',
-        },
-        {
-            id: 7,
-            src: 'https://fairyteller-dev.s3.ap-northeast-2.amazonaws.com/stickers/07.png',
-        },
-        {
-            id: 8,
-            src: 'https://fairyteller-dev.s3.ap-northeast-2.amazonaws.com/stickers/08.png',
-        },
-        {
-            id: 9,
-            src: 'https://fairyteller-dev.s3.ap-northeast-2.amazonaws.com/stickers/09.png',
-        },
-    ],
+  key: "SelectStickers",
+  default: [
+    {
+      id: 1,
+      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQshoMsYa1hFvjaMAwebiP2l99ssM_XWVrp7g&usqp=CAU",
+    },
+    {
+      id: 2,
+      src: "https://fairyteller-dev.s3.ap-northeast-2.amazonaws.com/stickers/02.png",
+    },
+    {
+      id: 3,
+      src: "https://fairyteller-dev.s3.ap-northeast-2.amazonaws.com/stickers/03.png",
+    },
+    {
+      id: 4,
+      src: "https://fairyteller-dev.s3.ap-northeast-2.amazonaws.com/stickers/04.png",
+    },
+    {
+      id: 5,
+      src: "https://fairyteller-dev.s3.ap-northeast-2.amazonaws.com/stickers/05.png",
+    },
+    {
+      id: 6,
+      src: "https://fairyteller-dev.s3.ap-northeast-2.amazonaws.com/stickers/06.png",
+    },
+    {
+      id: 7,
+      src: "https://fairyteller-dev.s3.ap-northeast-2.amazonaws.com/stickers/07.png",
+    },
+    {
+      id: 8,
+      src: "https://fairyteller-dev.s3.ap-northeast-2.amazonaws.com/stickers/08.png",
+    },
+    {
+      id: 9,
+      src: "https://fairyteller-dev.s3.ap-northeast-2.amazonaws.com/stickers/09.png",
+    },
+  ],
 });
 
 export const SaveState = atom({
-    key: 'SaveState',
-    default: 'none',
+  key: "SaveState",
+  default: "none",
 });
 
-export const Canvasexport = atom({ key: 'Canvasexport', default: [], effects_UNSTABLE: [persistAtom] });
+export const Canvasexport = atom({
+  key: "Canvasexport",
+  default: [],
+  effects_UNSTABLE: [persistAtom],
+});
