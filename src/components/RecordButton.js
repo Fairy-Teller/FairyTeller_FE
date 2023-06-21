d9e4ff10974c1469dda171a599273618aa665159
Kimjaejung <67777124+baebang@users.noreply.github.com>
2023-06-21 19:26:29 +0900

    [Add] 1차 커밋

--- src/components/RecordButton.js
+++ src/components/RecordButton.js
@@ -1,113 +1,136 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useState, useEffect } from 'react';
+import { sendAudioData } from '../service/FairytaleService';
 
 const buttonStyle = {
-  backgroundColor: "#70a5e3",
-  color: "#fff",
-  padding: "10px 20px",
-  border: "none",
-  borderRadius: "5px",
-  cursor: "pointer",
-  fontSize: "1rem",
-  transition: "background-color 0.3s ease",
-  marginRight: "10px",
+    backgroundColor: '#70a5e3',
+    color: '#fff',
+    padding: '10px 20px',
+    border: 'none',
+    borderRadius: '5px',
+    cursor: 'pointer',
+    fontSize: '1rem',
+    transition: 'background-color 0.3s ease',
+    marginRight: '10px',
 };
 
 const recordInfoStyle = {
-  marginTop: "10px",
-  fontWeight: "bold",
+    marginTop: '10px',
+    fontWeight: 'bold',
 };
 
-const RecordButton = ({
-  pageNumber,
-  onRecordingComplete,
-  initialAudioUrl,
-  audioRef,
-  onCloseAndRefresh,
-}) => {
-  const [isRecording, setIsRecording] = useState(false);
-  const [audioBlob, setAudioBlob] = useState(null);
-  const [shouldAutoPlay, setShouldAutoPlay] = useState(false);
-  const mediaRecorder = useRef(null);
+const RecordButton = ({ pageNumber, onRecordingComplete, initialAudioUrl, audioRef, onCloseAndRefresh, bookid }) => {
+    const [isRecording, setIsRecording] = useState(false);
+    const [audioBlob, setAudioBlob] = useState(null);
+    const [shouldAutoPlay, setShouldAutoPlay] = useState(false);
+    const mediaRecorder = useRef(null);
 
-  useEffect(() => {
-    if (shouldAutoPlay && audioRef.current) {
-      audioRef.current.play();
-      setShouldAutoPlay(false);
-    }
-  }, [shouldAutoPlay, audioRef]);
+    useEffect(() => {
+        if (shouldAutoPlay && audioRef.current) {
+            audioRef.current.play();
+            setShouldAutoPlay(false);
+        }
+    }, [shouldAutoPlay, audioRef]);
 
-  const handleStartRecording = async () => {
-    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-    mediaRecorder.current = new MediaRecorder(stream);
+    const handleStartRecording = async () => {
+        const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+        mediaRecorder.current = new MediaRecorder(stream);
 
-    const audioChunks = [];
-    mediaRecorder.current.ondataavailable = (event) => {
-      audioChunks.push(event.data);
+        const audioChunks = [];
+        mediaRecorder.current.ondataavailable = (event) => {
+            audioChunks.push(event.data);
+        };
+
+        mediaRecorder.current.onstop = () => {
+            const newAudioBlob = new Blob(audioChunks, { type: 'audio/wav' });
+            setAudioBlob(newAudioBlob);
+            onRecordingComplete(pageNumber, newAudioBlob);
+            audioRef.current.src = URL.createObjectURL(newAudioBlob);
+            setShouldAutoPlay(true);
+        };
+
+        mediaRecorder.current.start();
+        setIsRecording(true);
     };
 
-    mediaRecorder.current.onstop = () => {
-      const newAudioBlob = new Blob(audioChunks, { type: "audio/wav" });
-      setAudioBlob(newAudioBlob);
-      onRecordingComplete(pageNumber, newAudioBlob);
-      audioRef.current.src = URL.createObjectURL(newAudioBlob);
-      setShouldAutoPlay(true);
+    const handleStopRecording = () => {
+        mediaRecorder.current.stop();
+        setIsRecording(false);
     };
 
-    mediaRecorder.current.start();
-    setIsRecording(true);
-  };
+    const convertBlobToBase64 = (blob) => {
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.onloadend = () => resolve(reader.result);
+            reader.onerror = reject;
+            reader.readAsDataURL(blob);
+        });
+    };
 
-  const handleStopRecording = () => {
-    mediaRecorder.current.stop();
-    setIsRecording(false);
-  };
+    const handleSaveRecording = async () => {
+        try {
+            const base64Audio = await convertBlobToBase64(audioBlob);
+            console.log('bookid', bookid);
+            const payload = {
+                bookId: bookid,
+                pages: [
+                    {
+                        pageNo: pageNumber,
+                        audioUrl: base64Audio,
+                    },
+                ],
+            };
+            console.log(payload);
+            sendAudioData(payload);
 
-  const handleSaveRecording = async () => {
-    console.log("Saving to the backend server");
+            if (onCloseAndRefresh) {
+                onCloseAndRefresh();
+            }
+        } catch (error) {
+            console.error('Error converting audio blob to base64', error);
+        }
+    };
+    // const handleSaveRecording = async () => {
+    //     console.log('Saving to the backend server');
 
-    if (onCloseAndRefresh) {
-      onCloseAndRefresh();
-    }
-  };
+    //     if (onCloseAndRefresh) {
+    //         onCloseAndRefresh();
+    //     }
+    // };
 
-  return (
-    <div
-      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
-    >
-      <div style={{ display: "flex", alignItems: "center" }}>
-        {!isRecording && (
-          <button style={buttonStyle} onClick={handleStartRecording}>
-            녹음시작 {pageNumber}
-          </button>
-        )}
-        {audioBlob && (
-          <button style={buttonStyle} onClick={handleSaveRecording}>
-            저장
-          </button>
-        )}
-      </div>
-      {isRecording && (
-        <div>
-          <div style={recordInfoStyle}>녹음중입니다... {pageNumber}</div>
-          <button style={buttonStyle} onClick={handleStopRecording}>
-            녹음종료
-          </button>
+    return (
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+                {!isRecording && (
+                    <button style={buttonStyle} onClick={handleStartRecording}>
+                        녹음시작 {pageNumber}
+                    </button>
+                )}
+                {audioBlob && (
+                    <button style={buttonStyle} onClick={handleSaveRecording}>
+                        저장
+                    </button>
+                )}
+            </div>
+            {isRecording && (
+                <div>
+                    <div style={recordInfoStyle}>녹음중입니다... {pageNumber}</div>
+                    <button style={buttonStyle} onClick={handleStopRecording}>
+                        녹음종료
+                    </button>
+                </div>
+            )}
+
+            {/* Dropdown to select the version of audio if available */}
+            {initialAudioUrl && (
+                <select onChange={(e) => (audioRef.current.src = e.target.value)}>
+                    <option value={initialAudioUrl}>Original Audio</option>
+                    {audioBlob && <option value={URL.createObjectURL(audioBlob)}>User Audio</option>}
+                </select>
+            )}
+
+            <audio ref={audioRef} controls style={{ marginTop: '10px' }} />
         </div>
-      )}
-
-      {/* Dropdown to select the version of audio if available */}
-      {initialAudioUrl && (
-        <select onChange={(e) => (audioRef.current.src = e.target.value)}>
-          <option value={initialAudioUrl}>Original Audio</option>
-          {audioBlob && (
-            <option value={URL.createObjectURL(audioBlob)}>User Audio</option>
-          )}
-        </select>
-      )}
-
-      <audio ref={audioRef} controls style={{ marginTop: "10px" }} />
-    </div>
-  );
+    );
 };
 
 export default RecordButton;
--- src/pages/fairytale/FairytaleShow.js
+++ src/pages/fairytale/FairytaleShow.js
@@ -1,221 +1,201 @@
-import React, { useState, useEffect, useRef } from "react";
-import styled, { css, keyframes } from "styled-components";
-import { useLocation } from "react-router-dom";
-import RecordButton from "../../components/RecordButton";
-import { call } from "../../service/ApiService";
-import { sendAudioData } from "../../service/FairytaleService";
-import Modal from "react-modal";
+import React, { useState, useEffect, useRef } from 'react';
+import styled, { css, keyframes } from 'styled-components';
+import { useLocation } from 'react-router-dom';
+import RecordButton from '../../components/RecordButton';
+import { call } from '../../service/ApiService';
+import { sendAudioData } from '../../service/FairytaleService';
+import Modal from 'react-modal';
 
 const CenteredContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `;
 
 const CardContainer = styled.img`
-  width: 1250px;
-  height: 720px;
-  margin-left: 2%;
-  margin-right: 2%;
-  src: "/images/prev.png";
+    width: 1250px;
+    height: 720px;
+    margin-left: 2%;
+    margin-right: 2%;
+    src: '/images/prev.png';
 `;
 
 const AudioContainer = styled.div`
-  bottom: 0;
-  text-align: center;
-  padding: 20px;
+    bottom: 0;
+    text-align: center;
+    padding: 20px;
 `;
 
 const FairyPage = styled.div`
-  display: flex;
+    display: flex;
 `;
 
 const Arrow = styled.button`
-  ${(props) =>
-    props.disabled &&
-    css`
-      opacity: 0.5;
-      pointer-events: none;
-    `}
+    ${(props) =>
+        props.disabled &&
+        css`
+            opacity: 0.5;
+            pointer-events: none;
+        `}
 `;
 
 function FairytaleShow(bookid) {
-  const audioRef = useRef(null);
-  const [bookInfo, setBookInfo] = useState([]);
-  const [audioInfo, setAudioInfo] = useState([]);
-  const [userAudioInfo, setUserAudioInfo] = useState([]);
-  const [currentPage, setCurrentPage] = useState(0);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [recordingInProgress, setRecordingInProgress] = useState(false);
-  const [recordedAudioBlob, setRecordedAudioBlob] = useState(null);
+    const audioRef = useRef(null);
+    const [bookInfo, setBookInfo] = useState([]);
+    const [audioInfo, setAudioInfo] = useState([]);
+    const [userAudioInfo, setUserAudioInfo] = useState([]);
+    const [currentPage, setCurrentPage] = useState(0);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [recordingInProgress, setRecordingInProgress] = useState(false);
+    const [recordedAudioBlob, setRecordedAudioBlob] = useState(null);
+    const [check, setCheck] = useState('tts');
 
-  useEffect(() => {
-    if (bookid.props !== "") {
-      console.log(">>>>", bookid.props);
-      showBook(bookid.props);
-    }
-  }, [bookid]);
+    useEffect(() => {
+        if (bookid.props !== '') {
+            console.log('>>>>', bookid.props);
+            showBook(bookid.props);
+        }
+    }, [bookid]);
+    useEffect(() => {
+        if (bookid.props !== '') {
+            console.log('>>>>', bookid.props);
+            showBook(bookid.props);
+        }
+    }, [check]);
+    console.log('check', check);
 
-  const showBook = async (props) => {
-    try {
-      const bookinfos = await call("/book/getBookById", "POST", {
-        bookId: props,
-      });
-      const imgearr = [];
-      const audioarr = [];
-      const userAudioarr = [];
-      for (let i = 0; i < bookinfos.pages.length; i++) {
-        imgearr[i] = bookinfos.pages[i].finalImageUrl;
-        audioarr[i] = bookinfos.pages[i].audioUrl;
-        userAudioarr[i] = bookinfos.pages[i].userAudioUrl;
-      }
-      setBookInfo(imgearr);
-      setAudioInfo(audioarr);
-      setUserAudioInfo(userAudioarr);
-    } catch (error) {
-      console.log("Error fetching data:", error);
-    }
-  };
-
-  const handlePrevPage = () => {
-    if (currentPage > 0) {
-      setCurrentPage((prevPage) => prevPage - 1);
-    }
-  };
-
-  const handleNextPage = () => {
-    if (currentPage < bookInfo.length - 1) {
-      setCurrentPage((prevPage) => prevPage + 1);
-    }
-  };
-
-  const convertBlobToBase64 = (blob) => {
-    return new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      reader.onloadend = () => resolve(reader.result);
-      reader.onerror = reject;
-      reader.readAsDataURL(blob);
-    });
-  };
-
-  const handleRecordingComplete = async (pageNumber, audioBlob) => {
-    console.log("Recording completed for page:", pageNumber);
-    setRecordedAudioBlob(audioBlob);
-    setRecordingInProgress(false);
-
-    if (audioRef.current) {
-      const audioUrl = URL.createObjectURL(audioBlob);
-      audioRef.current.src = audioUrl;
-
-      const handleLoadedData = () => {
-        audioRef.current.play();
-        audioRef.current.removeEventListener("loadeddata", handleLoadedData);
-      };
-
-      audioRef.current.addEventListener("loadeddata", handleLoadedData);
-    }
-  };
-
-  const handleSaveRecording = async () => {
-    try {
-      const base64Audio = await convertBlobToBase64(recordedAudioBlob);
-      const payload = {
-        bookId: bookid.props,
-        pages: [
-          {
-            pageNo: currentPage + 1,
-            audioUrl: base64Audio,
-          },
-        ],
-      };
-      console.log(payload);
-      sendAudioData(payload);
-      setIsModalOpen(false);
-
-      showBook(bookid.props);
-    } catch (error) {
-      console.error("Error converting audio blob to base64", error);
-    }
-  };
-
-  useEffect(() => {
-    return () => {
-      if (audioRef.current) {
-        audioRef.current.pause();
-        audioRef.current.currentTime = 0;
-      }
+    const showBook = async (props) => {
+        try {
+            const bookinfos = await call('/book/getBookById', 'POST', {
+                bookId: props,
+            });
+            const imgearr = [];
+            const audioarr = [];
+            const userAudioarr = [];
+            for (let i = 0; i < bookinfos.pages.length; i++) {
+                imgearr[i] = bookinfos.pages[i].finalImageUrl;
+                audioarr[i] = bookinfos.pages[i].audioUrl;
+                userAudioarr[i] = bookinfos.pages[i].userAudioUrl;
+            }
+            setBookInfo(imgearr);
+            setAudioInfo(audioarr);
+            setUserAudioInfo(userAudioarr);
+            console.log('??????');
+        } catch (error) {
+            console.log('Error fetching data:', error);
+        }
     };
-  }, []);
 
-  return (
-    <CenteredContainer>
-      {bookInfo.length > 0 && (
-        <div>
-          <AudioContainer>
-            <audio
-              ref={audioRef}
-              src={audioInfo[currentPage]}
-              controls
-              preload="auto"
-            />
-            <button onClick={() => setIsModalOpen(true)}>녹음하기</button>
-            {userAudioInfo[currentPage] && (
-              <select
-                onChange={(e) => {
-                  if (audioRef.current) {
-                    audioRef.current.src = e.target.value;
-                    audioRef.current.load();
-                  }
-                }}
-              >
-                <option value={audioInfo[currentPage]}>Original Audio</option>
-                <option value={userAudioInfo[currentPage]}>User Audio</option>
-              </select>
-            )}
-          </AudioContainer>
-          <div>
-            {isModalOpen && (
-              <Modal
-                isOpen={isModalOpen}
-                onRequestClose={() => setIsModalOpen(false)}
-              >
-                <RecordButton
-                  pageNumber={currentPage + 1}
-                  onRecordingComplete={handleRecordingComplete}
-                  audioRef={audioRef}
-                  onCloseAndRefresh={() => {
-                    setIsModalOpen(false);
-                    showBook(bookid.props);
-                  }}
-                />
-              </Modal>
-            )}
-          </div>
+    const handlePrevPage = () => {
+        if (currentPage > 0) {
+            setCurrentPage((prevPage) => prevPage - 1);
+        }
+    };
 
-          <FairyPage>
-            <Arrow
-              className="prevButton"
-              disabled={currentPage === 0}
-              onClick={handlePrevPage}
-            >
-              <img src="/images/prev.png" />
-            </Arrow>
-            <img
-              src={bookInfo[currentPage]}
-              style={{ margin: "0 10px 0 10px" }}
-            />
-            <Arrow
-              className="nextButton"
-              disabled={currentPage === bookInfo.length - 1}
-              onClick={handleNextPage}
-            >
-              <img src="/images/next.png" />
-            </Arrow>
-          </FairyPage>
-        </div>
-      )}
-    </CenteredContainer>
-  );
+    const handleNextPage = () => {
+        if (currentPage < bookInfo.length - 1) {
+            setCurrentPage((prevPage) => prevPage + 1);
+        }
+    };
+
+    const handleRecordingComplete = async (pageNumber, audioBlob) => {
+        console.log('Recording completed for page:', pageNumber);
+        console.log('audioBlob', audioBlob);
+        setRecordedAudioBlob(audioBlob);
+        setRecordingInProgress(false);
+
+        if (audioRef.current) {
+            const audioUrl = URL.createObjectURL(audioBlob);
+            audioRef.current.src = audioUrl;
+
+            const handleLoadedData = () => {
+                audioRef.current.play();
+                audioRef.current.removeEventListener('loadeddata', handleLoadedData);
+            };
+
+            audioRef.current.addEventListener('loadeddata', handleLoadedData);
+        }
+    };
+    const convertBlobToBase64 = (blob) => {
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.onloadend = () => resolve(reader.result);
+            reader.onerror = reject;
+            reader.readAsDataURL(blob);
+        });
+    };
+
+    useEffect(() => {
+        return () => {
+            if (audioRef.current) {
+                audioRef.current.pause();
+                audioRef.current.currentTime = 0;
+            }
+        };
+    }, []);
+    console.log('currentPage', userAudioInfo[currentPage]);
+    return (
+        <CenteredContainer>
+            {bookInfo.length > 0 && (
+                <div>
+                    <AudioContainer>
+                        {check == 'tts' || check == 'user' ? (
+                            <audio
+                                ref={audioRef}
+                                src={check == 'tts' ? audioInfo[currentPage] : userAudioInfo[currentPage]}
+                                controls
+                                preload="auto"
+                            />
+                        ) : (
+                            <></>
+                        )}
+                        {!userAudioInfo[currentPage] && <button onClick={() => setIsModalOpen(true)}>녹음하기</button>}
+                        {
+                            <select
+                                onChange={(e) => {
+                                    setCheck(e.target.value);
+                                    console.log('check', check);
+                                }}
+                            >
+                                <option value={'tts'}>Original Audio</option>
+                                <option value={'user'}>User Audio</option>
+                            </select>
+                        }
+                    </AudioContainer>
+                    <div>
+                        {isModalOpen && (
+                            <Modal isOpen={isModalOpen} onRequestClose={() => setIsModalOpen(false)}>
+                                <RecordButton
+                                    pageNumber={currentPage + 1}
+                                    onRecordingComplete={handleRecordingComplete}
+                                    audioRef={audioRef}
+                                    onCloseAndRefresh={() => {
+                                        setIsModalOpen(false);
+                                        showBook(bookid.props);
+                                    }}
+                                    bookid={bookid.props}
+                                />
+                            </Modal>
+                        )}
+                    </div>
+
+                    <FairyPage>
+                        <Arrow className="prevButton" disabled={currentPage === 0} onClick={handlePrevPage}>
+                            <img src="/images/prev.png" />
+                        </Arrow>
+                        <img src={bookInfo[currentPage]} style={{ margin: '0 10px 0 10px' }} />
+                        <Arrow
+                            className="nextButton"
+                            disabled={currentPage === bookInfo.length - 1}
+                            onClick={handleNextPage}
+                        >
+                            <img src="/images/next.png" />
+                        </Arrow>
+                    </FairyPage>
+                </div>
+            )}
+        </CenteredContainer>
+    );
 }
 
 export default FairytaleShow;
