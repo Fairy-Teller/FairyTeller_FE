0210aee1f4565d3353df02e210defc83856951e2
flyduck-dev <flyduck0dev@gmail.com>
2023-06-23 06:03:05 +0900

    [Add]서버데이터 이미지 로딩

--- src/pages/fairytale/Book.js
+++ src/pages/fairytale/Book.js
@@ -5,68 +5,23 @@ import React from 'react';
 import PageFlip from 'react-pageflip';
 
 
-function Book() {
+function Book({ bookInfo }) {
+    const imageUrls = bookInfo;
     return (
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
             <PageFlip width={515} height={580}>
-                {/* 첫 번째 페이지 */}
-                <div className="flip-paper-page">
-                    <div className="half-image-container">
-                        <img src={"https://s3.ap-northeast-2.amazonaws.com/fairyteller-dev/1103_1.png"} alt="Left Half" className="left-half-image" />
+            {imageUrls.flatMap((url, i) => [
+                    <div className="flip-paper-page" key={`left-${i}`}>
+                        <div className="half-image-container">
+                            <img src={url} alt={`Left Half ${i+1}`} className="left-half-image" />
+                        </div>
+                    </div>,
+                    <div className="flip-paper-page" key={`right-${i}`}>
+                        <div className="half-image-container">
+                            <img src={url} alt={`Right Half ${i+1}`} className="right-half-image" />
+                        </div>
                     </div>
-                </div>
-                <div className="flip-paper-page">
-                    <div className="half-image-container">
-                        <img src={"https://s3.ap-northeast-2.amazonaws.com/fairyteller-dev/1103_1.png"} alt="Right Half" className="right-half-image" />
-                    </div>
-                </div>
-
-                {/* 두 번째 페이지 */}
-                <div className="flip-paper-page">
-                    <div className="half-image-container">
-                        <img src={"https://s3.ap-northeast-2.amazonaws.com/fairyteller-dev/1103_2.png"} alt="Left Half" className="left-half-image" />
-                    </div>
-                </div>
-                <div className="flip-paper-page">
-                    <div className="half-image-container">
-                        <img src={"https://s3.ap-northeast-2.amazonaws.com/fairyteller-dev/1103_2.png"} alt="Right Half" className="right-half-image" />
-                    </div>
-                </div>
-
-                {/* 세 번째 페이지 */}
-                <div className="flip-paper-page">
-                    <div className="half-image-container">
-                        <img src={"https://s3.ap-northeast-2.amazonaws.com/fairyteller-dev/1103_3.png"} alt="Left Half" className="left-half-image" />
-                    </div>
-                </div>
-                <div className="flip-paper-page">
-                    <div className="half-image-container">
-                        <img src={"https://s3.ap-northeast-2.amazonaws.com/fairyteller-dev/1103_3.png"} alt="Right Half" className="right-half-image" />
-                    </div>
-                </div>
-
-                {/* 네 번째 페이지 */}
-                <div className="flip-paper-page">
-                    <div className="half-image-container">
-                        <img src={"https://s3.ap-northeast-2.amazonaws.com/fairyteller-dev/1103_4.png"} alt="Left Half" className="left-half-image" />
-                    </div>
-                </div>
-                <div className="flip-paper-page">
-                    <div className="half-image-container">
-                        <img src={"https://s3.ap-northeast-2.amazonaws.com/fairyteller-dev/1103_4.png"} alt="Right Half" className="right-half-image" />
-                    </div>
-                </div>
-                {/* 다섯 번째 페이지 */}
-                <div className="flip-paper-page">
-                    <div className="half-image-container">
-                        <img src={"https://s3.ap-northeast-2.amazonaws.com/fairyteller-dev/1103_5.png"} alt="Left Half" className="left-half-image" />
-                    </div>
-                </div>
-                <div className="flip-paper-page">
-                    <div className="half-image-container">
-                        <img src={"https://s3.ap-northeast-2.amazonaws.com/fairyteller-dev/1103_5.png"} alt="Right Half" className="right-half-image" />
-                    </div>
-                </div>
+                ])}
             </PageFlip>
         </div>
     );
