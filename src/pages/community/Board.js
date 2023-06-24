cd38de56570831bc72d3c347d862d528a8ee2a62
flyduck-dev <flyduck0dev@gmail.com>
2023-06-24 22:49:30 +0900

    [Add]게시판 검색 이중클릭수정

--- src/pages/community/Board.js
+++ src/pages/community/Board.js
@@ -8,69 +8,58 @@ import ContentCover from "../../components/global/ContentCover";
 import InnerCover from "../../components/global/InnerCover";
 import BookContainer from "../../components/global/BookContainer";
 import Book from "../../components/global/Book";
-
 const Board = () => {
   const [currentPage, setCurrentPage] = useState(0);
   const [books, setBooks] = useState([]);
   const [totalPages, setTotalPages] = useState(0);
   const [keyword, setKeyword] = useState("");
   const [searchType, setSearchType] = useState("");
-
-  const fetchData = useCallback(async () => {
-    try {
-      let endpoint = "/board";
-      let params = `?page=${currentPage}&size=8`;
-
-      if (searchType === "author") {
-        params += `&author=${encodeURIComponent(keyword)}`;
-      } else if (searchType === "title") {
-        params += `&title=${encodeURIComponent(keyword)}`;
-      } else {
-        params += `&keyword=${encodeURIComponent(keyword)}`;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        let endpoint = "/board";
+        let params = `?page=${currentPage}&size=8`;
+        if (searchType === "author") {
+          params += `&author=${encodeURIComponent(keyword)}`;
+        } else if (searchType === "title") {
+          params += `&title=${encodeURIComponent(keyword)}`;
+        } else {
+          params += `&keyword=${encodeURIComponent(keyword)}`;
+        }
+        const response = await call(endpoint + params, "GET", null);
+        console.log("Board API Response:", response); // API 응답 확인
+        if (response && response.data) {
+          console.log("Board Data:", response.data); // 데이터 확인
+          setBooks(response.data);
+          setTotalPages(response.totalPages);
+        }
+      } catch (error) {
+        console.log("Error fetching data:", error);
       }
-
-      const response = await call(endpoint + params, "GET", null);
-      console.log("Board API Response:", response); // API 응답 확인
-      if (response && response.data) {
-        console.log("Board Data:", response.data); // 데이터 확인
-        setBooks(response.data);
-        setTotalPages(response.totalPages);
-      }
-    } catch (error) {
-      console.log("Error fetching data:", error);
-    }
-  }, [currentPage, keyword, searchType]);
-
-  useEffect(() => { 
+    };
     fetchData();
-  }, [fetchData, currentPage, keyword]);
-
+  }, [currentPage, keyword, searchType]);
   const handlePageChange = (pageNumber) => {
     setCurrentPage(pageNumber);
   };
-
   const handleSearch = async (keyword, searchType) => {
     try {
       setKeyword(keyword);
       setSearchType(searchType);
       setCurrentPage(0); // 검색어가 변경되면 페이지 번호를 0으로 초기화
-      fetchData(); // fetchData 함수 호출하여 데이터 가져오기
     } catch (error) {
       console.log("Error fetching data:", error);
     }
   };
-  
   const truncateTitle = (title) => {
     if (title.length > 10) {
       return title.substring(0, 10) + "...";
     }
     return title;
   };
-
   useEffect(() => {
     console.log("Books:", books);
   }, [books]);
-
   return (
     <Container>
       <Header mode={"default"} />
@@ -123,5 +112,4 @@ const Board = () => {
     </Container>
   );
 };
-
 export default Board;
