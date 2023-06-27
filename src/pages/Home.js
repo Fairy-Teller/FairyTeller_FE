a26b52372947c1dd38a304d390188d253facf664
helen1031 <32950301+helen1031@users.noreply.github.com>
2023-06-27 15:44:38 +0900

    [Refactor] 액세스토큰 있을 시 start로 이동

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import styled, { keyframes } from 'styled-components';
-import '../css/home.css';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import styled, { keyframes } from "styled-components";
+import "../css/home.css";
 
 const Home = () => {
-    const [showMain, setShowMain] = useState(false);
-    const navigation = useNavigate();
+  const [showMain, setShowMain] = useState(false);
+  const navigation = useNavigate();
 
-    const ImagesContainer = styled.div`
-        position: relative;
-        display: flex;
-        justify-content: center; /* Center horizontally */
+  const ImagesContainer = styled.div`
+    position: relative;
+    display: flex;
+    justify-content: center; /* Center horizontally */
 
-        width: 100vw;
-        height: 100vh;
-    `;
+    width: 100vw;
+    height: 100vh;
+  `;
 
-    const slideAnimation = keyframes`
+  const slideAnimation = keyframes`
     0% {
       transform: translateX(0%);
     }
@@ -24,25 +24,25 @@ const Home = () => {
       transform: translateX(-20%);
     }
   `;
-    const ImageLogo = styled.img`
-        filter: drop-shadow(5px 5px 5px #000);
-        position: absolute;
-        width: 100%;
-        height: 100vh;
-        object-fit: cover;
+  const ImageLogo = styled.img`
+    filter: drop-shadow(5px 5px 5px #000);
+    position: absolute;
+    width: 100%;
+    height: 100vh;
+    object-fit: cover;
 
-        opacity: 0;
-        animation: containerFadeIn 1s ease-in forwards;
-    `;
-    const Image = styled.img`
-        filter: drop-shadow(5px 5px 5px #000);
-        position: absolute;
-        width: 100%;
-        height: 100vh;
-        object-fit: cover;
-    `;
+    opacity: 0;
+    animation: containerFadeIn 1s ease-in forwards;
+  `;
+  const Image = styled.img`
+    filter: drop-shadow(5px 5px 5px #000);
+    position: absolute;
+    width: 100%;
+    height: 100vh;
+    object-fit: cover;
+  `;
 
-    const containerFadeIn = keyframes`
+  const containerFadeIn = keyframes`
        0% {
       opacity: 0;
       transform: translateY(-25px);
@@ -54,61 +54,73 @@ const Home = () => {
 
   `;
 
-    const BackImage = styled.div`
-        filter: drop-shadow(5px 5px 5px #000);
-        position: absolute;
-        width: 200%;
-        height: 100vh;
-        object-fit: cover;
-        animation: ${slideAnimation} 30s infinite linear;
-        background-repeat: repeat;
-        background-image: url('images/CartoonStarryNight.png');
-    `;
+  const BackImage = styled.div`
+    filter: drop-shadow(5px 5px 5px #000);
+    position: absolute;
+    width: 200%;
+    height: 100vh;
+    object-fit: cover;
+    animation: ${slideAnimation} 30s infinite linear;
+    background-repeat: repeat;
+    background-image: url("images/CartoonStarryNight.png");
+  `;
 
-    const StartButton = styled.button`
-        /* Star 1 */
+  const StartButton = styled.button`
+    /* Star 1 */
 
-        box-sizing: border-box;
+    box-sizing: border-box;
 
-        position: absolute;
-        width: 207px;
-        height: 40px;
+    position: absolute;
+    width: 207px;
+    height: 40px;
 
-        /* 시작하기 */
+    /* 시작하기 */
 
-        color: #ffffff;
-        font-size: large;
-        margin-top: 15%;
-        background: linear-gradient(180deg, #fee080 0%, rgba(254, 224, 128, 0) 100%);
-        box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-        border-radius: 22px;
-        animation: containerFadeIn 1s ease-in forwards;
-        z-index: 4;
-    `;
-    const gotoLogin = () => {
-        navigation('/Login');
-    };
-
-    useEffect(() => {
-        setShowMain(true);
-        document.body.style.overflow = 'hidden';
-        return () => {
-            document.body.style.overflow = 'auto';
-        };
-    }, []);
-
-    return (
-        <ImagesContainer>
-            {showMain && (
-                <>
-                    <StartButton onClick={gotoLogin}>시작하기</StartButton>
-                    <BackImage src="images/CartoonStarryNight.png" style={{ zIndex: 1 }} />
-                    <Image src="images/girl.png" style={{ zIndex: 2 }} />
-                    <ImageLogo src="images/logo_home.png" style={{ zIndex: 3 }} />
-                </>
-            )}
-        </ImagesContainer>
+    color: #ffffff;
+    font-size: large;
+    margin-top: 15%;
+    background: linear-gradient(
+      180deg,
+      #fee080 0%,
+      rgba(254, 224, 128, 0) 100%
     );
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 22px;
+    animation: containerFadeIn 1s ease-in forwards;
+    z-index: 4;
+  `;
+  const gotoLogin = () => {
+    const token = localStorage.getItem("ACCESS_TOKEN");
+    if (token) {
+      navigation("/start");
+    } else {
+      navigation("/Login");
+    }
+  };
+
+  useEffect(() => {
+    setShowMain(true);
+    document.body.style.overflow = "hidden";
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, []);
+
+  return (
+    <ImagesContainer>
+      {showMain && (
+        <>
+          <StartButton onClick={gotoLogin}>시작하기</StartButton>
+          <BackImage
+            src="images/CartoonStarryNight.png"
+            style={{ zIndex: 1 }}
+          />
+          <Image src="images/girl.png" style={{ zIndex: 2 }} />
+          <ImageLogo src="images/logo_home.png" style={{ zIndex: 3 }} />
+        </>
+      )}
+    </ImagesContainer>
+  );
 };
 
 export default Home;
