98ec852a9b79f6d39628b3c04327ed8cc019769e
flyduck-dev <flyduck0dev@gmail.com>
2023-07-01 16:05:49 +0900

    [Add] 인기게시물 css

--- src/pages/community/PopularBoard.js
+++ src/pages/community/PopularBoard.js
@@ -1,18 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../../css/PopularBoard.css';
 
 const PopularBoard = ({ board }) => {
     return (
-        <div className="popular-board">
-            <img src={board.thumbnailUrl} alt={board.title} className="board-image"/>
-            <h3 className="board-title">{board.title}</h3>
-            <p className="board-price">{board.price}</p>
-            <p className="board-region-name">{board.location}</p>
-            <div className="board-counts">
-                <span>좋아요 {board.likes}</span> ∙ <span>댓글 {board.chats}</span>
+        <Link to={`/board/${board.boardId}`} style={{ textDecoration: 'none' }}>
+            <div className="popular-board">
+                <img src={board.thumbnailUrl} alt={board.title} className="board-image"/>
+                <h3 className="board-title">{board.title}</h3>
+                <div className="board-counts">
+                    <span>좋아요 {board.likes}</span> ∙ <span>댓글 {board.chats}</span>
+                </div>
             </div>
-        </div>
+        </Link>
     );
 };
 
 export default PopularBoard;
+
