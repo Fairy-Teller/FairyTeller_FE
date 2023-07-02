f0339bdd4b15db0f4bdb6274962ac820c28a25fe
flyduck-dev <flyduck0dev@gmail.com>
2023-07-02 16:42:58 +0900

    [Add]title css

--- src/pages/community/Board.js
+++ src/pages/community/Board.js
@@ -90,8 +90,12 @@ const Board = () => {
             fontSize: "2.8rem",
             textAlign: "left",
             cursor: "pointer",
+            transition: "font-size 0.3s ease",
           }}
-          onClick={handleBoardTitleClick}>
+          onClick={handleBoardTitleClick}
+          onMouseEnter={(e) => e.target.style.fontSize = "2.9rem"}
+          onMouseLeave={(e) => e.target.style.fontSize = "2.8rem"} 
+          >
           우리들의 도서관
         </div>
         <BoardSearch handleSearch={handleSearch} />
