90fd088100f9dfea79bf059e969f5d140c132fc2
Haebin <hbtopkr@gmail.com>
2023-07-03 19:15:31 +0900

    [Feat] SignUp background image add

--- src/pages/user/SignUp.js
+++ src/pages/user/SignUp.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { signup } from "../../service/UserService";
-import { Link } from "react-router-dom";
 import { API_BASE_URL } from "../../api-config";
+import LazyBackground from "../../components/common/LazyBackground";
+import base64_Bg from "../../script/BASE64_Bg";
 
 import "../../css/signup.css";
 
@@ -19,12 +20,9 @@ function SignUp() {
         alert("아이디를 입력해주세요");
         return;
       }
-      const response = await axios.get(
-        API_BASE_URL + "/auth/signup/check-userid",
-        {
-          params: { userid: userInputUserId },
-        }
-      );
+      const response = await axios.get(API_BASE_URL + "/auth/signup/check-userid", {
+        params: { userid: userInputUserId },
+      });
       setIsUidAvailable(response.data);
     } catch (error) {
       console.error("There was an error!", error);
@@ -42,12 +40,9 @@ function SignUp() {
         alert("별명을 입력해주세요");
         return;
       }
-      const response = await axios.get(
-        API_BASE_URL + "/auth/signup/check-nickname",
-        {
-          params: { nickname: userInputNickname },
-        }
-      );
+      const response = await axios.get(API_BASE_URL + "/auth/signup/check-nickname", {
+        params: { nickname: userInputNickname },
+      });
       setIsNicknameAvailable(response.data);
     } catch (error) {
       console.error("There was an error!", error);
@@ -82,78 +77,92 @@ function SignUp() {
       return;
     }
 
-    signup({ userid: userid, nickname: nickname, password: password }).then(
-      (response) => {
-        window.location.href = "/login";
-      }
-    );
+    signup({ userid: userid, nickname: nickname, password: password }).then((response) => {
+      window.location.href = "/login";
+    });
   };
 
   return (
-    <div id="background">
-      <div id="container">
-        <form noValidate onSubmit={handleSubmit}>
+    <div id='background'>
+      <LazyBackground
+        type='default'
+        src='https://ik.imagekit.io/hbcho/StarryNight_start.jpg'
+        placeholder={base64_Bg}
+      />
+      <div id='container'>
+        <form
+          noValidate
+          onSubmit={handleSubmit}>
           <h1>계정 생성</h1>
 
-          <div class="signup-input-container">
-            <label htmlFor="userid">아이디</label>
+          <div class='signup-input-container'>
+            <label htmlFor='userid'>아이디</label>
             <input
-              type="text"
-              id="userid"
-              name="userid"
+              type='text'
+              id='userid'
+              name='userid'
               autoFocus
               onChange={(e) => setUserInputUserId(e.target.value)}
               required
             />
-            <button onClick={checkUidAvailability} type="button">
+            <button
+              onClick={checkUidAvailability}
+              type='button'>
               중복확인
             </button>
             {isUidAvailable === false && <p>이미 존재하는 아이디입니다</p>}
             {isUidAvailable === true && <p>사용가능한 아이디입니다</p>}
             {isUidAvailable === null && <p>중복확인 버튼을 입력해주세요</p>}
           </div>
-          <div class="signup-input-container">
-            <label htmlFor="password">패스워드</label>
-            <input type="password" id="password" name="password" required />
-          </div>
-          <div class="signup-input-container">
-            <label htmlFor="confirmPassword">패스워드 확인</label>
+          <div class='signup-input-container'>
+            <label htmlFor='password'>패스워드</label>
             <input
-              type="password"
-              id="confirmPassword"
-              name="confirmPassword"
+              type='password'
+              id='password'
+              name='password'
+              required
+            />
+          </div>
+          <div class='signup-input-container'>
+            <label htmlFor='confirmPassword'>패스워드 확인</label>
+            <input
+              type='password'
+              id='confirmPassword'
+              name='confirmPassword'
               value={confirmPassword}
               onChange={(e) => setConfirmPassword(e.target.value)}
               required
             />
           </div>
-          <div class="signup-input-container">
-            <label htmlFor="nickname">닉네임</label>
+          <div class='signup-input-container'>
+            <label htmlFor='nickname'>닉네임</label>
             <input
-              type="text"
-              id="nickname"
-              name="nickname"
+              type='text'
+              id='nickname'
+              name='nickname'
               onChange={(e) => setUserInputNickname(e.target.value)}
               required
             />
-            <button onClick={checkNicknameAvailability} type="button">
+            <button
+              onClick={checkNicknameAvailability}
+              type='button'>
               중복확인
             </button>
             {isNicknameAvailable === false && <p>이미 존재하는 별명입니다</p>}
             {isNicknameAvailable === true && <p>사용가능한 별명입니다</p>}
-            {isNicknameAvailable === null && (
-              <p>중복확인 버튼을 입력해주세요</p>
-            )}
+            {isNicknameAvailable === null && <p>중복확인 버튼을 입력해주세요</p>}
           </div>
           <div>
-            <button class="submit" type="submit">
+            <button
+              class='submit'
+              type='submit'>
               계정 생성
             </button>
           </div>
         </form>
 
-        <div class="linkContainer">
-          <a href="/login">이미 계정이 있습니까? 로그인 하세요</a>
+        <div class='linkContainer'>
+          <a href='/login'>이미 계정이 있습니까? 로그인 하세요</a>
         </div>
       </div>
     </div>
