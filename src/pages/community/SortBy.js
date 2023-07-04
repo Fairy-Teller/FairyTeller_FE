a5de7d91dd4e8f307f2ee8a2af83f653cd77d2fa
flyduck-dev <flyduck0dev@gmail.com>
2023-07-05 03:32:25 +0900

    [Add]정렬 스타일

--- src/pages/community/SortBy.js
+++ src/pages/community/SortBy.js
@@ -14,12 +14,12 @@ const SortBy = ({ handleSort }) => {
         <option value="createdDatetime">최신순</option>
         <option value="likes">좋아요순</option>
       </select>
-      <FontAwesomeIcon icon={faSort} style={styles.icon} />
+      <span style={styles.icon}>
+        <FontAwesomeIcon icon={faSort} />
+      </span>
     </div>
   );
-};
-
-const styles = {
+};const styles = {
   container: {
     display: "flex",
     justifyContent: "flex-start",
@@ -29,12 +29,12 @@ const styles = {
   },
   select: {
     width: "170px",
-    padding: "10px",
+    padding: "9px",
     border: "none",
     borderRadius: "5px",
     boxShadow: "0 2px 4px rgba(0,0,0,0.1)",
     backgroundColor: "#f1f1f1",
-    color: "#333",
+    color: "#666",
     fontSize: "16px",
     appearance: "none",
     outline: "none",
