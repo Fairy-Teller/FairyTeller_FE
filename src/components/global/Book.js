db6fa18fd75a2e027a3d48d024fa4cbb5e899524
flyduck-dev <flyduck0dev@gmail.com>
2023-07-05 19:10:06 +0900

    [Add]반응형 대처

--- src/components/global/Book.js
+++ src/components/global/Book.js
@@ -2,7 +2,7 @@ import React, { memo } from "react";
 import { Link } from "react-router-dom";
 import BookSub from "./BookSub";
 import LazyImage from "../common/LazyImage";
-import '../../css/BoardBook.css'; // 이 라인이 CSS 파일을 import 합니다.
+import '../../css/BoardBook.css';
 
 const Book = ({ book, truncateTitle, linkPath, idProperty, showSub }) => {
   const title = truncateTitle ? truncateTitle(book.title) : book.title;
@@ -29,8 +29,11 @@ const Book = ({ book, truncateTitle, linkPath, idProperty, showSub }) => {
           <div className='book__cover'>
             <div className='book__page book__page--front'>
               <div className='book__content'>
+              <div className='book__title__container'>
                 <h4 className='book__title'>{title}</h4>
+                </div>
                 {showSub && (
+                  <div className='book__sub__container'>
                   <BookSub
                     className='book__sub'
                     book={book}
@@ -39,12 +42,15 @@ const Book = ({ book, truncateTitle, linkPath, idProperty, showSub }) => {
                     likeCount={likeCount}
                     comments={comments}
                   />
+                  </div>
                 )}
-                <LazyImage
-                  className='book__thumbnail'
-                  src={book.thumbnailUrl}
-                  alt={title + " created by " + author}
-                />
+                <div className='book__thumbnail__container'>
+                  <LazyImage
+                   className='book__thumbnail'
+                   src={book.thumbnailUrl}
+                   alt={title + " created by " + author}
+                 />
+               </div>
               </div>
             </div>
           </div>
