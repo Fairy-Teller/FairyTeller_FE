4853ef6650982ff3ea78526a59c3cbaabf8f2866
flyduck-dev <flyduck0dev@gmail.com>
2023-07-06 17:13:43 +0900

    [Add]Board css

--- src/components/global/BookSub.js
+++ src/components/global/BookSub.js
@@ -5,12 +5,10 @@ import { faHeart } from "@fortawesome/free-solid-svg-icons";
 const BookSub = ({ book, formattedDate, author, likeCount, comments }) => {
   return (
     <div className="book__info">
-      <div className="sub-info">
-        <span className="separator">·</span>
-        <span>{comments ? `${comments.length}개의 댓글` : ""}</span>
-      </div>
       <div className="author-likes">
-        <span className="author-name">{author}</span>
+        <div className="author">
+          <span className="author-name">{author}</span>
+        </div>
         <div className="likes">
           <FontAwesomeIcon icon={faHeart} className="heart-icon" />
           <span className="like-count">{likeCount ? likeCount : ""}</span>
@@ -19,5 +17,6 @@ const BookSub = ({ book, formattedDate, author, likeCount, comments }) => {
     </div>
   );
 };
+
 
 export default BookSub;
